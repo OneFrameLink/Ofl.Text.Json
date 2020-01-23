caa324217399c00795a8667ea092e44b78bd43c7
casperOne <casperOne@caspershouse.com>
2020-01-23 16:49:06 -0500

    Initial implementation.

--- src/Ofl.Text.Json/ExceptionExtensions.cs
+++ src/Ofl.Text.Json/ExceptionExtensions.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Ofl.Reflection;
 using System.Text.Json;
+using Ofl.Reflection;
 
-namespace Ofl.Serialization.Json.Newtonsoft
+namespace Ofl.Text.Json
 {
     internal static class ExceptionExtensions
     {
@@ -24,45 +24,51 @@ namespace Ofl.Serialization.Json.Newtonsoft
             if (options == null) throw new ArgumentNullException(nameof(options));
 
             // Create the stack of exceptions to write.
-            var stack = new Stack<Exception?>();
+            var stack = new Stack<(Action? pre, Exception? exception, Action? post)>();
 
             // Push the original exception on.
-            stack.Push(value);
+            stack.Push((null, value, null));
 
             // While there are items on the stack.
             while (stack.Any())
             {
                 // Pop the item.
-                value = stack.Pop();
+                var (pre, exception, post) = stack.Pop();
 
-                // If there is no exception, write null and be done with it.
-                if (value == null)
+                // Is there a pre?  Execute if there is one.
+                pre?.Invoke();
+
+                // If there is no exception, write null.
+                if (exception == null)
                 {
                     // Write null.
                     writer.WriteNullValue();
+
+                    // If there is a post action, do it.
+                    post?.Invoke();
 
                     // Continue.
                     continue;
                 }
 
-                // Write object values.
+                // Start writing the object.
                 writer.WriteStartObject();
 
                 // TODO: Serialize the Data property on
                 // Exception, if there are values.                
 
                 // Write properties.
-                writer.WriteString(nameof(Exception.HelpLink), value.HelpLink);
-                writer.WriteNumber(nameof(Exception.HResult), value.HResult);
-                writer.WriteString(nameof(Exception.Message), value.Message);
-                writer.WriteString(nameof(Exception.Source), value.Source);
-                writer.WriteString(nameof(Exception.StackTrace), value.StackTrace);
+                writer.WriteString(nameof(Exception.HelpLink), exception.HelpLink);
+                writer.WriteNumber(nameof(Exception.HResult), exception.HResult);
+                writer.WriteString(nameof(Exception.Message), exception.Message);
+                writer.WriteString(nameof(Exception.Source), exception.Source);
+                writer.WriteString(nameof(Exception.StackTrace), exception.StackTrace);
 
                 // Need to write the type shim.  Write the meta property
                 writer.WritePropertyName("$Meta");
 
                 // Write all the type information.
-                Type type = value.GetType();
+                Type type = exception.GetType();
 
                 // Write a new object.
                 writer.WriteStartObject();
@@ -86,11 +92,37 @@ namespace Ofl.Serialization.Json.Newtonsoft
                     {
                         // Get the value.
                         value = (Exception?) propertyInfo.GetValue(value);
+
+                        // Write the property name.
+                        writer.WritePropertyName(propertyInfo.Name);
+
+                        // Push on the stack.
+                        stack.Push((null, value, writer.WriteEndObject));
+
+                        // Continue.
+                        continue;
+                    }
+
+                    // If this is assignable to an IEnumerable<Exception> then
+                    // get that.
+                    if (EnumerableExceptionTypeInfo.IsAssignableFrom(propertyInfo.PropertyType))
+                    {
+                        // Get the value.
+                        IEnumerable<Exception>? exceptions = (IEnumerable<Exception>?) propertyInfo.GetValue(value);
+
+                        // Write the property name.
+                        writer.WritePropertyName(propertyInfo.Name);
+
+                        // Cycle through the items.
+                        stack.Push((value, writer.WriteEndObject));
+
+                        // Continue.
+                        continue;
                     }
                 }
-
-                // Close the object.
-                writer.WriteEndObject();
+                
+                // Call the post action.
+                post();
             }
         }
     }
