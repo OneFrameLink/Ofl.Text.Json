097b234a02c259d53cdb8894677cab2d076125cb
casperOne <casperOne@caspershouse.com>
2020-02-15 22:04:02 -0500

    Added snake case naming policy.

---
+++ src/Ofl.Text.Json/SnakeCaseJsonNamingPolicy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Ofl.Text.Json
+{
+    public class SnakeCaseJsonNamingPolicy : JsonNamingPolicy
+    {
+        #region Constructor
+
+        public SnakeCaseJsonNamingPolicy(
+            bool lowercaseParts
+        )
+        {
+            // Assign values.
+            _lowercaseParts = lowercaseParts;
+        }
+
+        #endregion
+
+        #region Instance, read-only state
+
+        private readonly bool _lowercaseParts;
+
+        #endregion
+
+        #region Overrides
+
+        public override string ConvertName(string name)
+        {
+            // Validate parameters.
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+
+            // Allocate locally.  Allocate two times the length
+            // of the string that came in.
+            Span<char> buffer = stackalloc char[name.Length * 2];
+
+            // The current index in the span.
+            int index = 0;
+
+            // Cycle through the characters.
+            foreach (char c in name)
+            {
+                // Is this upper case?
+                if (char.IsUpper(c))
+                {
+                    // If this is not the first character, add an
+                    // underscore.
+                    if (index > 0)
+                        buffer[index++] = '_';
+
+                    // Lowercase if lowercasing the parts.
+                    buffer[index++] = _lowercaseParts
+                        ? char.ToLower(c)
+                        : c;
+                }
+                else
+                    // Add the buffer.
+                    buffer[index++] = c;
+            }
+
+            // Slice the buffer.
+            buffer = buffer.Slice(0, index);
+
+            // Return a new string.
+            return new string(buffer);
+        }
+
+        #endregion
+    }
+}
---
+++ test/Ofl.Text.Json.Tests/SnakeCaseNamingPolicyTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace Ofl.Text.Json.Tests
+{
+    public class SnakeCaseNamingPolicyTests
+    {
+        [Theory]
+        [InlineData("Hello", "Hello", false)]
+        [InlineData("Hello", "hello", true)]
+        [InlineData("HelloWorld", "Hello_World", false)]
+        [InlineData("HelloWorld", "hello_world", true)]
+        public void Test_Convert(string test, string expected, bool lowercaseParts)
+        {
+            // Validate parameters.
+            if (string.IsNullOrWhiteSpace(test))
+                throw new ArgumentNullException(nameof(test));
+            if (string.IsNullOrWhiteSpace(expected))
+                throw new ArgumentNullException(nameof(expected));
+
+            // Create the policy.
+            var policy = new SnakeCaseJsonNamingPolicy(lowercaseParts);
+
+            // Convert.
+            string actual = policy
+                .ConvertName(test);
+
+            // They are equal.
+            Assert.Equal(expected, actual);
+        }
+    }
+}
