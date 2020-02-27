6f9b10fafb9116fceb9eda907a59b6a90910af19
casperOne <casperOne@caspershouse.com>
2020-02-27 15:19:08 -0500

    First crack at immutable dictionary serializer.

---
+++ src/Ofl.Text.Json/ImmutableDictionaryJsonConverter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ofl.Text.Json
+{
+    public class ImmutableDictionaryJsonConverter<TKey, TValue> : JsonConverter<IImmutableDictionary<TKey, TValue>>
+        where TKey : notnull
+    {
+        #region Static constructor.
+
+        static ImmutableDictionaryJsonConverter()
+        {
+            // Get the type converter for the key.
+            _typeConverter = TypeDescriptor.GetConverter(typeof(TKey));
+        }
+
+        #endregion
+
+        #region Static, read only members
+
+        private static readonly TypeConverter _typeConverter;
+
+        #endregion
+
+        #region Overrides
+
+        public override IImmutableDictionary<TKey, TValue> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            // Validate parameters.
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            // Is this null?  If so, return null.
+            if (reader.TokenType == JsonTokenType.Null) return null;
+
+            // If it is not an object, throw.
+            if (reader.TokenType != JsonTokenType.StartObject)
+                throw new JsonException($"Expected a token type of {nameof(JsonTokenType.StartObject)}, encountered {reader.TokenType}.");
+
+            // The return value.
+            IImmutableDictionary<TKey, TValue> values = ImmutableDictionary<TKey, TValue>.Empty;
+
+            // Cycle.
+            while (reader.Read() && reader.TokenType != JsonTokenType.EndObject)
+            {
+                // We should be on the name now.  Get the string.
+                string stringKey = reader.GetString();
+
+                // Move the reader now.
+                if (!reader.Read())
+                    throw new JsonException($"Expected call to {nameof(Utf8JsonReader.Read)} to return true, returned false.");
+
+                // Convert the key.
+                TKey key = (TKey) _typeConverter.ConvertFromInvariantString(stringKey);
+
+                // Deserialize the value.
+                TValue value = JsonSerializer.Deserialize<TValue>(ref reader, options);
+
+                // Add.
+                values = values.Add(key, value);
+            }
+
+            // Return the values.
+            return values;
+        }
+
+        public override void Write(Utf8JsonWriter writer, IImmutableDictionary<TKey, TValue> value, JsonSerializerOptions options)
+        {
+            // Validate parameters.
+            if (writer == null) throw new ArgumentNullException(nameof(writer));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            
+            // If the value is null, write null and get out.
+            if (value == null)
+            {
+                // Write null and get out.
+                writer.WriteNullValue();
+                return;
+            }
+
+            // Write the start object.
+            writer.WriteStartObject();
+
+            // Cycle through the pairs.
+            foreach (KeyValuePair<TKey, TValue> pair in value)
+            {
+                // Get the key property.
+                string key = _typeConverter.ConvertToInvariantString(null!, pair.Key);
+
+                // Run through the dictionary json policy, if there is one.
+                key = options.DictionaryKeyPolicy?.ConvertName(key) ?? key;
+
+                // Write the property and the value.
+                writer.WritePropertyName(key);
+
+                // Write the value.
+                JsonSerializer.Serialize(pair.Value, typeof(TValue), options);
+            }
+
+            // Close the object off.
+            writer.WriteEndObject();
+        }
+
+        #endregion
+    }
+}
---
+++ test/Ofl.Text.Json.Tests/ImmutableDictionaryJsonConverterFactoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Immutable;
+using System.Text.Json;
+using Xunit;
+
+namespace Ofl.Text.Json.Tests
+{
+    public class ImmutableDictionaryJsonConverterFactoryTests
+    {
+        #region Helpers
+
+        public static JsonSerializerOptions CreateJsonSerializerOptions() =>
+            new JsonSerializerOptions {
+                Converters = { new ImmutableDictionaryJsonConverterFactory() }
+            };
+
+        #endregion
+
+        #region Tests
+
+        [Fact]
+        public void Test_ImmutableDictionary_With_Non_String_Key_Serialization()
+        {
+            // Create the options.
+            JsonSerializerOptions options = CreateJsonSerializerOptions();
+
+            // Serialize a test with numbers.
+            const string json = "{ \"1\": \"hello\", \"2\": \"there\" }";
+
+            // Serialize.
+            ImmutableDictionary<int, string> deserialized =
+                JsonSerializer.Deserialize<ImmutableDictionary<int, string>>(json, options);
+
+            // Validate the keys.
+            Assert.Equal(2, deserialized.Count);
+            Assert.Equal("hello", deserialized[1]);
+            Assert.Equal("there", deserialized[2]);
+        }
+
+        #endregion
+    }
+}
